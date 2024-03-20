2853c7a7ae5afc84550fb63ed8a91805d34be0c1
blue892 <110934835+blue892@users.noreply.github.com>
2024-03-20 00:19:42 -0400

    Create grid and onevent that changes square color

---
+++ script.js
@@ -0,0 +1,18 @@
+let container = document.querySelector("#container");
+
+for (let i = 0; i < 16; i++) {
+    let row = document.createElement("div");
+    row.style.display = "flex";
+    row.style.margin = "0px";
+    row.style.padding = "0px";
+    for (let j = 0; j < 16; j++) {
+        let square = document.createElement("div");
+        square.style.aspectRatio = "1 / 1";
+        square.style.flex = "1 1 50px";
+        row.appendChild(square);
+        square.addEventListener("mouseover", (e) => {
+            square.style.backgroundColor = "black";
+        });
+    }
+    container.appendChild(row);
+}
