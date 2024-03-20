802c56ae5e88bda334e9b4e34c72cf14fb1e51f1
blue892 <110934835+blue892@users.noreply.github.com>
2024-03-20 00:30:54 -0400

    Add styles.css and header

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         let square = document.createElement("div");
         square.style.aspectRatio = "1 / 1";
-        square.style.flex = "1 1 50px";
+        square.style.flex = "1 1 20px";
         row.appendChild(square);
         square.addEventListener("mouseover", (e) => {
             square.style.backgroundColor = "black";
